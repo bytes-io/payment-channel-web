d7725d7bb991702475b1e4241bc90c53411eb618
phani <pasupulaphani@gmail.com>
2018-07-29 23:57:15 +0100

    fix deploy to local env

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const PaymentChannel = artifacts.require('./PaymentChannel.sol');
 
-module.exports = function (deployer) {
-  deployer.deploy(PaymentChannel);
+const now = new Date();
+const twoHoursLater = new Date(now.getTime() + (2 * 1000 * 60 * 60)).getTime() / 1000;
+
+module.exports = function (deployer, network, accounts) {
+  console.log(accounts, twoHoursLater)
+  deployer.deploy(PaymentChannel, accounts[0], twoHoursLater);
 };
